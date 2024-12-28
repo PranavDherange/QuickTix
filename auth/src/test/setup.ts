5c9a9ce65286b7b24790270a88d79c5aba180562
Pranav <prn2108@gmail.com>
2024-12-28 15:39:15 +0530

    feat: unit tests for auth service

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,12 +1,18 @@
 // mongo = await MongoMemoryServer.create();
 // const mongoUri = mongo.getUri();
 
+import request from 'supertest';
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
 import { app } from "../app";
 
+declare global {
+  var signin: () => Promise<string[]>;
+}
+
 let mongo: any;
 beforeAll(async () => {
+  process.env.JWT_KEY = 'asdfasdfas';
   mongo = await MongoMemoryServer.create();
   const mongoUri = mongo.getUri();
 
@@ -29,3 +35,21 @@ afterAll(async () => {
   }
   await mongoose.connection.close();
 });
+
+
+global.signin = async () => {
+  const email = 'test@test.com';
+  const password = 'password';
+
+  const response = await request(app)
+      .post('/api/users/signup')
+      .send({
+        email: 'test@test.com',
+        password: 'password'
+      })
+    .expect(201);
+  
+  const cookie = response.get('Set-Cookie') as string[];
+
+  return cookie;
+}
