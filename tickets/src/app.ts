32c6566668d72e975e9efbc2fadcedbe0ee84988
Pranav <prn2108@gmail.com>
2025-01-31 10:12:15 +0530

    feat: setup fo tickets service and writing some tests before development

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,6 +2,8 @@ import express, { Request, Response, NextFunction } from "express";
 import 'express-async-errors';
 import { json } from "body-parser";
 import cookieSession from "cookie-session";
+import { createTicketRouter } from './routes/new'
+import { errorHandler, NotFoundError, currentUser } from "@pdquicktixx/common";
 
 const app = express();
 app.set('trust proxy', true);
@@ -12,6 +14,8 @@ app.use(
         secure: process.env.NODE_ENV !== 'test'
     })
 )
+app.use(currentUser)
+app.use(createTicketRouter);
 
 app.all('*', async (req, res) => {
     throw new NotFoundError();
