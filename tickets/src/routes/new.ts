cf7df3d4ebb040363c13117b74af7773092608c2
Pranav <prn2108@gmail.com>
2025-02-04 01:31:10 +0530

    feat: complete tickets service with unit test cases

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,11 +1,11 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import { requireAuth, validateRequest } from "@pdquicktixx/common";
-
+import { Ticket } from '../models/ticket';
 
 const router = express.Router();
 
-router.post('/api/tickets', requireAuth,
+router.post('/api/tickets',
   requireAuth,
   [
     body('title').not().isEmpty().withMessage('Title is required'),
@@ -14,8 +14,17 @@ router.post('/api/tickets', requireAuth,
       .withMessage('Price must be greater than 0'),
   ],
   validateRequest,
-  (req: Request, res: Response) => {
-  res.sendStatus(200);
-});
+  async (req: Request, res: Response) => {
+    const { title, price } = req.body;
+
+    const ticket = Ticket.build({
+      title,
+      price,
+      userId: req.currentUser!.id
+    });
+    await ticket.save();
+
+    res.status(201).send(ticket);
+  });
 
 export { router as createTicketRouter };
